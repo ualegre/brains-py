ed5fa84f6e98f7d452374f7fefd09e65663344bf
ualegre <u.alegre@utwente.nl>
2020-03-19 17:35:18 +0100

    Minor renaming on the utils input

--- bspyproc/utils/input.py
+++ bspyproc/utils/input.py
@@ -32,11 +32,15 @@ def generate_sinewave(freq, t, amplitude, fs, phase=np.zeros(7)):
 def normalise(x, eps=1e-5):
     return ((x - np.mean(x)) / (np.sqrt(np.var(x) + eps)))
 
+def get_current_to_voltage_vars(v_min, v_max, i_min, i_max):
+    scale = ((v_min - v_max) / (i_min - i_max))
+    offset = v_max - scale * i_max
+    return scale, offset
 
 def map_to_voltage(x, v_min, v_max):
-    a = ((v_min - v_max) / (x.min() - x.max()))
-    b = v_max - a * x.max()
-    return (a * x) + b
+    scale = ((v_min - v_max) / (x.min() - x.max()))
+    offset = v_max - scale * x.max()
+    return (scale * x) + offset
 
 def get_map_to_voltage_vars(v_min, v_max, x=np.array([-1,1])):
     scale = ((v_min - v_max) / (x.min() - x.max()))
